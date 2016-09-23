57b4d2d5af0e87d488f6c4a9190b2ec3faee699c
MizukiNakano <mizuki590@gmail.com>
2016-09-23 18:44:45 +0900

    add notification

---
+++ app/src/main/java/sikmi/firebasesample/NotificationService.java
@@ -0,0 +1,36 @@
+package sikmi.firebasesample;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by nakanomizuki on 2016/09/23.
+ */
+public class NotificationService extends FirebaseMessagingService {
+    private String TAG = "firebase";
+
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
+}
